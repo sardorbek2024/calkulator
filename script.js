9efb6cd510ae2c3ce296b23f17b6d881cf8840b0
sardorbek2024 <168269584+sardorbek2024@users.noreply.github.com>
2024-08-29 11:24:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let a = +prompt( "1 число")
+let b = prompt("числитель")
+let c = +prompt("2 число")
+
+if( b == "+"){
+    alert( a + c )
+}else if( b == "-"){
+    alert( a - c )
+}else if( b == "*"){
+    alert( a * c )
+}else if( b == "/"){
+    alert( a / c )
+}else{
+    alert("динаху пидор")
+}
